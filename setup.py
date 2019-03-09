36805924aa71296d714b16fb1a143eadcc67a71f
Ben Nuttall <ben@bennuttall.com>
2019-03-09 01:02:11 +0000

    Add metadata to setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,28 @@ setup(
     version="0.1.0",
     author="Ben Nuttall",
     description="Take questions posted on a particular Stack Exchange site, generate a meme out of it and tweet it",
-    license="BSD",
-    keywords=[],
-    url="",
+    license="OSI Approved :: BSD License",
+    keywords=[
+        stackexchange,
+        stackoverflow,
+        meme,
+        twitter,
+    ],
+    url="https://github.com/bennuttall/meme-overflow",
     packages=find_packages(),
     requires=[
         'requests',
         'twython',
     ],
     long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Games/Entertainment",
+    ]
 )
