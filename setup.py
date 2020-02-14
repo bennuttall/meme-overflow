fc89f1ea167246277201345c5222f60615bf0f67
Ben Nuttall <ben@bennuttall.com>
2020-02-14 22:17:51 +0000

    Move setup vars from __init__ to setup.py

--- setup.py
+++ setup.py
@@ -10,26 +10,62 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 if not sys.version_info >= (3, 6):
     raise RuntimeError('This application requires Python 3.6 or later')
 
+__project__ = 'memeoverflow'
+__version__ = '0.6.0'
+__author__ = 'Ben Nuttall'
+__author_email__ = 'ben@bennuttall.com'
+__url__ = 'https://github.com/bennuttall/meme-overflow'
+__platforms__ = ['ALL']
+__requires__ = [
+    'requests',
+    'twython',
+    'logzero',
+]
+__extra_requires__ = {
+    'test':  ['pytest', 'coverage', 'mock'],
+}
+__python_requires__ = '>=3.6'
+__keywords__ = [
+    'twitter',
+    'stackoverflow',
+    'stackexchange',
+    'meme',
+    'imgflip',
+]
+__classifiers__ = [
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: BSD License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Topic :: Games/Entertainment",
+]
+__license__ = [
+    c.rsplit('::', 1)[1].strip()
+    for c in __classifiers__
+    if c.startswith('License ::')
+][0]
 
 def main():
     "Executes setup when this script is the top-level"
-    import memeoverflow as app
 
     with io.open(os.path.join(HERE, 'description.rst'), 'r') as description:
         setup(
-            name=app.__project__,
-            version=app.__version__,
-            author=app.__author__,
-            author_email=app.__author_email__,
-            url=app.__url__,
-            platforms=app.__platforms__,
-            requires=app.__requires__,
-            python_requires=app.__python_requires__,
-            keywords=app.__keywords__,
-            classifiers=app.__classifiers__,
-            install_requires=app.__requires__,
-            extra_requires=app.__extra_requires__,
-            description=app.__doc__,
+            name=__project__,
+            version=__version__,
+            author=__author__,
+            author_email=__author_email__,
+            url=__url__,
+            platforms=__platforms__,
+            requires=__requires__,
+            python_requires=__python_requires__,
+            keywords=__keywords__,
+            classifiers=__classifiers__,
+            install_requires=__requires__,
+            extra_requires=__extra_requires__,
+            description=__doc__,
             long_description=description.read(),
             packages=find_packages(),
         )
